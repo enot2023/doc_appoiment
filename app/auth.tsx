b2d4d27c2ec09c72e29861b2649b7ec456d55eba
enot2023 <soramjoykumar2023@gmail.com>
2024-02-21 21:53:45 +0530

    forth-commit

--- app/auth.tsx
+++
@@ -1,7 +0,0 @@
-import StyleFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
-import FirebaseConfig from "@/components/FirebasseConfig/FirebaseConfig"
-
-// const uiConfig = {
-//     signInSuccessURL: "/",
-//     signInOption:[firebase.auth.githubAuthProvider]
-// }
